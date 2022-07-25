5498dd0d24e75499be9c0f10713fdf36371dc382
sparky1306 <sparky1306@gmail.com>
2022-07-25 23:51:41 +0000

    Title, nav-bar, footer, animation scaling added.

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// var boxOne = document.getElementsByClassName('box')[0];
-
-// document.getElementsByClassName('toggleButton')[0].onclick = function() {
-//   if(this.innerHTML === 'Play') 
-//   { 
-//     this.innerHTML = 'Pause';
-//     boxOne.classList.add('horizTranslate');
-//   } else {
-//     this.innerHTML = 'Play';
-//     var computedStyle = window.getComputedStyle(boxOne),
-//         marginLeft = computedStyle.getPropertyValue('margin-left');
-//     boxOne.style.marginLeft = marginLeft;
-//     boxOne.classList.remove('horizTranslate');    
-//   }  
-// }
-
 let statusVar = 'charge';
 let charge = document.getElementById('charge');
 let discharge = document.getElementById('discharge');
