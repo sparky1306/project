6dd6fcad7a379337702b592dd6d8ce010b33bc11
sparky1306 <sparky1306@gmail.com>
2022-08-02 20:36:59 -0600

    Hide quiz when results are shown

--- quiz.js
+++ quiz.js
@@ -1,4 +1,5 @@
 let quizForm = document.getElementById('quizForm');
+let quizResult = document.getElementById('quizResult');
 
 quizForm.onsubmit = function() {
   q1 = parseInt(document.querySelector('input[name = "q1"]:checked').value);
@@ -9,5 +10,19 @@ quizForm.onsubmit = function() {
   result = q1 + q2 + q3 + q4;
   document.getElementById('correct').innerHTML = result + "/4";
   document.getElementById('score').innerHTML = result / 4 * 100 + "%";
+
+  if (result === 4) {
+    document.getElementById('resultMessage').innerHTML = "Fantastic!  You aced it!!!";
+  }
+  else if (result >= 3) {
+    document.getElementById('resultMessage').innerHTML = "Not bad!  Good job!";
+  }
+  else {
+    document.getElementById('resultMessage').innerHTML = "Hmm... You'd better go through the lesson again.";
+  }
+
+  quizResult.style.display = 'grid';
+  quizForm.style.display = 'none';
+
   return false;
 }
